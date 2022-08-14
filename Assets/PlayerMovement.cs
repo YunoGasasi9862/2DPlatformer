bcbd2dc1d110914bf08c7702744e88a4e8e37afb
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2022-08-14 14:09:09 +0300

    ss

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,12 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float CharacterSpeed = 50f;
+    public float jumpspeed = 10f;
+    bool isontheGround = true;
+    [SerializeField] Rigidbody2D rb;
 
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        float Horizontal = Input.GetAxis("Horizontal") * Time.deltaTime;
-        float Vertical = Input.GetAxis("Vertical") * Time.deltaTime;
+        float Horizontal = Input.GetAxis("Horizontal") * CharacterSpeed * Time.deltaTime;
 
-        transform.Translate(Horizontal, Vertical, 0);
+        transform.Translate(Horizontal, 0,0);
+
+        if (Input.GetButtonDown("Jump") && isontheGround)
+        {
+            rb.AddForce(Vector3.up * jumpspeed, ForceMode2D.Impulse);
+            rb.gravityScale = 1.5f;
+            isontheGround = false;
+
+            //another method is this:
+            //rb.velocity=new vector3(0,speed,0);
+        }
+
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+           if(collision.collider.tag=="Ground")
+        {
+            rb.gravityScale = 1;
+            isontheGround = true;
+        }
     }
 }
